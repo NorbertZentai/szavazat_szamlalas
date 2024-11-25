4c68023ff1dd2d24f01415714276e0416ae6eb22
Norbert Zentai <zentai.norbert96@gmail.com>
2024-11-25 16:49:16 +0100

    spring alkalmazás feltöltése

---
+++ szavazatszamlalo/src/test/java/hu/adatbazis/szavazatszamlalo/SzavazatszamlaloApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.adatbazis.szavazatszamlalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzavazatszamlaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
